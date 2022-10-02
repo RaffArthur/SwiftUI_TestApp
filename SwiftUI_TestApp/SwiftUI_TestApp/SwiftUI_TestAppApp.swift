d6fe02a3ea64ab35c93cbc7087bc20cf8c91b1ae
Arthur Raff <arthur.raff@icloud.com>
2022-10-02 14:32:20 +0300

    Project created

---
+++ SwiftUI_TestApp/SwiftUI_TestApp/SwiftUI_TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TestAppApp.swift
+//  SwiftUI_TestApp
+//
+//  Created by Arthur Raff on 02.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
